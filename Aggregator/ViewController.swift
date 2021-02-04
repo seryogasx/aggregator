9208e407776bdef59a5de8dd94fee7d7c7c2eac1
seryogasx <sergio.petrovx@yandex.ru>
2021-02-04 12:19:55 +0300

    initial commit

---
+++ Aggregator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aggregator
+//
+//  Created by Сергей Петров on 04.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
