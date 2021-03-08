419fb9107ac03d61919ec9ab222dcc987ad16f0e
seryogasx <sergio.petrovx@yandex.ru>
2021-03-08 20:14:15 +0300

    add vk page model

--- Aggregator/Model/Vk/VkMedia/VkPage.swift
+++ Aggregator/Model/Vk/VkMedia/VkPage.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 struct VkPage: Codable {
-    
+    let id: Int
+    let group_id: Int
+    let creator_id: Int?
+    let title: Int
+    let current_user_can_edit: Int?
+    let current_user_can_edit_access: Int?
+    let who_can_edit: Int?
+    let edited: Int?
+    let created: Int?
+    let editor_id: Int?
+    let views: Int?
+    let parent: String?
+    let parent2: String?
+    let source: String?
+    let html: String?
+    let view_url: String?
 }
